b528ea0e8632d76800f661a7591eba80b7089348
Anton <anton.dahlstrom@hotmail.com>
2024-10-01 10:54:19 +0200

    completed manager

--- Manager.cs
+++ Manager.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace OOPHotel2
 {
-    internal class Manager(string department, string name, int age, string employeeId, DateTime startDate, decimal salary) : Person(name, age, employeeId, startDate, salary)
+    internal class Manager(string department,  string name, int age, string employeeId, DateTime startDate, decimal salary) : Person(name, age, employeeId, startDate, salary)
     {
-        
+        string Department = department;
+        public void HoldMeeting()
+        {
+            Console.WriteLine($"Manager {Name} is holding a meeting.");
+        }
     }
 }
